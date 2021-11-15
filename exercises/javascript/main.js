ca5c6bcb863ff9f94d579f1fd138bc646e64b119
jessiekil <kile984@newschool.edu>
2021-11-14 22:33:08 -0500

    edit

--- exercises/javascript/main.js
+++ exercises/javascript/main.js
@@ -1,10 +1,26 @@
-
 let body = document.body;
+let button = document.querySelector(".button");
+let buttonTwo =  document.querySelector(".button-2");
+let buttonThree =  document.querySelector(".button-3");
+
+button.addEventListener("click", turnOnLight);
+buttonTwo.addEventListener("click", turnonLight);
+buttonThree.addEventListener("click", turnonLight1);
+
 
 function turnOnLight() {
   body.classList.toggle("light");
   button.classList.toggle("buttonOn");
 }
 
-let button = document.querySelector(".button");
-button.addEventListener("click", turnOnLight);
+
+function turnonLight() {
+  body.classList.toggle("light1");
+  button.classList.toggle("buttonOn1");
+}
+
+
+function turnonLight1() {
+  body.classList.toggle("light2");
+  button.classList.toggle("buttonOn2");
+}
